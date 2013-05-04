c9e48c7722317cd537b2e2baac2e12b514fcd4fa
Siva Jagadeesan <sivajag@gmail.com>
2013-05-03 23:55:22 -0700

    Added ant dependency to fix "java.lang.ClassNotFoundException: org.apache.tools.tar.TarOutputStream" exception

--- project.clj
+++ project.clj
@@ -3,4 +3,13 @@
   :url "httpd://github.com/palletops/lein-dist"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :plugins [[lein-swank "1.4.4"]
+            [lein-pprint "1.1.1"]
+            [lein-clojars "0.9.1"]]
+  
+  :dependencies [[org.apache.ant/ant "1.8.2"]]
+
+  :min-lein-version "2.0.0"
+
   :eval-in-leiningen true)
